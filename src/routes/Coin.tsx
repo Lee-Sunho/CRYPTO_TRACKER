123b678b77904d831bccb9ff9f6fed0610d53d7d
Lee-Sunho <sunho9889@soongsil.ac.kr>
2023-03-12 22:24:36 +0900

    feat: useRouteMatch 사용하여 현재 탭 표시하는 기능 구현

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { Link, Switch, Route, useLocation, useParams } from 'react-router-dom';
+import { Link, Switch, Route, useLocation, useParams, useRouteMatch } from 'react-router-dom';
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
 import Chart from './Chart';
@@ -57,7 +57,7 @@ const Tabs = styled.div`
   gap: 10px;
 `;
 
-const Tab = styled.span`
+const Tab = styled.span<{ isActive: Boolean }>`
   text-align: center;
   text-transform: uppercase;
   font-size: 12px;
@@ -65,6 +65,7 @@ const Tab = styled.span`
   background-color: rgba(0, 0, 0, 0.5);
   padding: 7px 0px;
   border-radius: 10px;
+  color: ${(props) => (props.isActive ? props.theme.accentColor : props.theme.textColor)};
   a {
     display: block;
   }
@@ -140,6 +141,8 @@ const Coin = () => {
   const [priceInfo, setPriceInfo] = useState<PriceData>();
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
+  const priceMatch = useRouteMatch('/:coinId/price');
+  const chartMatch = useRouteMatch('/:coinId/chart');
 
   useEffect(() => {
     (async () => {
@@ -194,10 +197,10 @@ const Coin = () => {
             </Route>
           </Switch>
           <Tabs>
-            <Tab>
+            <Tab isActive={chartMatch !== null}>
               <Link to={`/${coinId}/chart`}>Chart</Link>
             </Tab>
-            <Tab>
+            <Tab isActive={priceMatch !== null}>
               <Link to={`/${coinId}/price`}>Price</Link>
             </Tab>
           </Tabs>
